d4390329f1c1f7f9b885ccaef0313880beda49ef
arbaevsherbolot <arbaevsherbolot1@gmail.com>
2024-08-02 14:15:46 +0600

    some updates

--- src/app/redux/api/me/server.ts
+++ src/app/redux/api/me/server.ts
@@ -1,30 +1,29 @@
 'use server';
 
 import { unstable_noStore as noStore } from 'next/cache';
-import { cookies, headers as requestHeaders } from 'next/headers';
-import { userAgent as getUserAgent } from 'next/server';
+import { cookies, headers } from 'next/headers';
 
 export async function getMe(_request: GetMeRequest): Promise<GetMeResponse | undefined> {
   const session = cookies().get('session');
 
   if (!session) return;
 
-  const { os, device } = getUserAgent({ headers: requestHeaders() });
-  const userAgent = `${os.name} ${os.version} (${device.vendor}, ${device.model})`;
-  const xff = `${(cookies().get('x-forwarded-for')?.value ?? '127.0.0.1').split(',')[0]}`;
+  // const { os, device } = getUserAgent({ headers: requestHeaders() });
+  // const userAgent = `${os.name} ${os.version} (${device.vendor}, ${device.model})`;
+  // const xff = `${(cookies().get('x-forwarded-for')?.value ?? '127.0.0.1').split(',')[0]}`;
 
-  const headers = new Headers();
+  // const headers = new Headers();
 
-  headers.append('x-forwarded-for', xff);
-  headers.append('user-agent', userAgent);
-  headers.append('cookie', `session=${session.value}`);
+  // headers.append('x-forwarded-for', xff);
+  // headers.append('user-agent', userAgent);
+  // headers.append('cookie', `session=${session.value}`);
 
   noStore();
 
   try {
     const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/me`, {
       method: 'GET',
-      headers,
+      headers: headers(),
       credentials: 'include',
     });
     if (!response.ok) return;
