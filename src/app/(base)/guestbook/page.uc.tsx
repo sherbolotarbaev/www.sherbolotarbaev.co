8052a2ba601ec625e12a6fb6450312148196049b
arbaevsherbolot <arbaevsherbolot1@gmail.com>
2024-08-02 14:29:45 +0600

    some updates

--- src/app/(base)/guestbook/page.uc.tsx
+++ src/app/(base)/guestbook/page.uc.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import clsx from 'clsx';
-import { getCookie } from 'cookies-next';
 
 import Button from '~/app/components/button';
 
@@ -12,20 +11,18 @@ interface GuestbookClientProps {
 }
 
 export default function GuestbookClient({ me }: Readonly<GuestbookClientProps>) {
-  const ip = getCookie('x-forwarded-for');
-
   return (
     <>
       <div className={clsx('wrapper', styles.wrapper)}>
         <div className={clsx('container', styles.container)}>
           <h2 className="title">sign my guestbook</h2>
 
-          <p className="desc">
-            Your IP {ip} | IP from server {me?.metaData.ip}
-          </p>
-
           {me ? (
-            <p className="desc">Welcome back {me.name}!</p>
+            <p className="desc">
+              Welcome back {me.name}! I see {"you're"} currently in {me.metaData.city},{' '}
+              {me.metaData.country}. Your device: {me.metaData.device} | timezone:{' '}
+              {me.metaData.timezone}
+            </p>
           ) : (
             <Button
               redirect={`${process.env.NEXT_PUBLIC_AUTH_URL}/sign-in?next=/guestbook`}
--- src/app/redux/api/me/server.ts
+++ src/app/redux/api/me/server.ts
@@ -2,13 +2,18 @@
 
 import { unstable_noStore as noStore } from 'next/cache';
 import { cookies, headers } from 'next/headers';
+import { userAgent as getUserAgent } from 'next/server';
 
 export async function getMe(_request: GetMeRequest): Promise<GetMeResponse | undefined> {
   const session = cookies().get('session');
-
   if (!session) return;
 
   noStore();
+
+  const { os, device } = getUserAgent({ headers: headers() });
+  const userAgent = `${os.name} ${os.version} (${device.vendor}, ${device.model})`;
+
+  headers().set('user-agent', userAgent);
 
   try {
     const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/me`, {
