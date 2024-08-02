a1183b5c099bda3dd5bd94ecfa11ddf2b393e7c0
arbaevsherbolot <arbaevsherbolot1@gmail.com>
2024-08-02 14:35:45 +0600

    some updates

--- src/app/redux/api/me/server.ts
+++ src/app/redux/api/me/server.ts
@@ -1,7 +1,7 @@
 'use server';
 
 import { unstable_noStore as noStore } from 'next/cache';
-import { cookies, headers } from 'next/headers';
+import { cookies, headers as requestHeaders } from 'next/headers';
 import { userAgent as getUserAgent } from 'next/server';
 
 export async function getMe(_request: GetMeRequest): Promise<GetMeResponse | undefined> {
@@ -10,15 +10,16 @@ export async function getMe(_request: GetMeRequest): Promise<GetMeResponse | und
 
   noStore();
 
-  const { os, device } = getUserAgent({ headers: headers() });
+  const { os, device } = getUserAgent({ headers: requestHeaders() });
   const userAgent = `${os.name} ${os.version} (${device.vendor}, ${device.model})`;
 
-  headers().set('user-agent', userAgent);
-
   try {
+    const headers = new Headers(requestHeaders());
+    headers.set('user-agent', userAgent);
+
     const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/me`, {
       method: 'GET',
-      headers: Object.fromEntries(headers()),
+      headers,
       credentials: 'include',
     });
     if (!response.ok) return;
