f37315b98fd387b16cae5bbddde70ee281899933
arbaevsherbolot <arbaevsherbolot1@gmail.com>
2024-08-04 04:09:56 +0600

    add: vercel analytics & speed-insights

--- src/app/layout.uc.tsx
+++ src/app/layout.uc.tsx
@@ -1,5 +1,8 @@
 'use client';
 
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
 import TopLoadingBar from 'nextjs-toploader';
 import { Toaster } from 'sonner';
 import Providers from './providers';
@@ -14,6 +17,8 @@ export default function RootLayoutClient({ children }: Readonly<RootLayoutClient
       <TopLoadingBar color="var(--blue-color)" showSpinner={false} height={3} />
       <Providers>{children}</Providers>
       <Toaster />
+      <Analytics />
+      <SpeedInsights />
     </>
   );
 }
